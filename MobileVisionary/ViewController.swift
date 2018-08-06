b6848799839952065a8686f0c49cc36421039656
Jinnie Park <jinnie.park@appian.com>
2018-08-06 14:29:37 -0400

    first commit

---
+++ MobileVisionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileVisionary
+//
+//  Created by Jinnie Park on 8/6/18.
+//  Copyright © 2018 Jinnie Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MobileVisionaryTests/MobileVisionaryTests.swift
@@ -0,0 +1,36 @@
+//
+//  MobileVisionaryTests.swift
+//  MobileVisionaryTests
+//
+//  Created by Jinnie Park on 8/6/18.
+//  Copyright © 2018 Jinnie Park. All rights reserved.
+//
+
+import XCTest
+@testable import MobileVisionary
+
+class MobileVisionaryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
