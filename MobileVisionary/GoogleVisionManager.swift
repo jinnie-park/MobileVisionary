e183f7d47c57fdaae5e3fd138532726ac205970f
Jinnie Park <jinnie.park@appian.com>
2018-08-07 11:28:12 -0400

    google vision manager debugged

--- MobileVisionary/GoogleVisionManager.swift
+++ MobileVisionary/GoogleVisionManager.swift
@@ -27,17 +27,10 @@ class GoogleVisionManager: NSObject {
     }
     
     private func base64EncodeImage(_ image: UIImage) -> String {
-        if var imagedata = UIImagePNGRepresentation(image){
-            // Resize the image if it exceeds the 2MB API limit
-            if (imagedata.count > 2097152) {
-                let oldSize: CGSize = image.size
-                let newSize: CGSize = CGSize(width: 800, height: oldSize.height / oldSize.width * 800)
-                imagedata = resizeImage(newSize, image: image)
-            }
-            return imagedata.base64EncodedString(options: .endLineWithCarriageReturn)
-
-        }
-        return ""
+        let oldSize: CGSize = image.size
+        let newSize: CGSize = CGSize(width: 800, height: oldSize.height / oldSize.width * 800)
+        let imagedata = resizeImage(newSize, image: image)
+        return imagedata.base64EncodedString(options: .endLineWithCarriageReturn)
     }
     
     private func resizeImage(_ imageSize: CGSize, image: UIImage) -> Data {
