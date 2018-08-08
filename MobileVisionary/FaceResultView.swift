4e19dbf9214a3fda4dd26dddba0b4ea8afe15fd1
Bowen Sun <bowen.sun@appian.com>
2018-08-08 13:09:26 -0400

    Foundations of Tableview

---
+++ MobileVisionary/FaceResultView.swift
@@ -0,0 +1,45 @@
+//
+//  FaceResultView.swift
+//  MobileVisionary
+//
+//  Created by Bowen Sun on 8/8/18.
+//  Copyright © 2018 Jinnie Park. All rights reserved.
+//
+
+import UIKit
+
+class FaceResultView: UIView {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func awakeFromNib() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension FaceResultView: UITableViewDataSource, UITableViewDelegate{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Strange"
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
---
+++ MobileVisionary/TextResultView.swift
@@ -0,0 +1,37 @@
+//
+//  TextResultView.swift
+//  MobileVisionary
+//
+//  Created by Bowen Sun on 8/8/18.
+//  Copyright © 2018 Jinnie Park. All rights reserved.
+//
+
+import UIKit
+
+class TextResultView: UIView {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func awakeFromNib() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+}
+
+extension TextResultView: UITableViewDelegate, UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
